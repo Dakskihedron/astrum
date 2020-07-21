e2ae6886191acdc2b53918d492a9c1962e0418c6
Dakski <ghasthunter352@gmail.com>
2020-07-21 22:45:51 +1200

    Minor changes to descriptions and responses

--- cogs/misc.py
+++ cogs/misc.py
@@ -10,7 +10,7 @@ class Misc(commands.Cog):
         self.bot = bot
 
     # Shows information about a user
-    @commands.command(name='userinfo', usage='[user]', brief='Shows information about a user.', description='Shows information about a user.')
+    @commands.command(name='userinfo', usage='[user]', brief='Shows information about a user.', description='Shows information about a user. Leave blank to show information about yourself.')
     @commands.guild_only()
     async def user_info(self, ctx, user: discord.Member = None):
         user = user or ctx.author
@@ -29,10 +29,10 @@ class Misc(commands.Cog):
     @user_info.error
     async def user_info_error(self, ctx, error):
         if isinstance(error, commands.BadArgument):
-            return await ctx.send(f"{ctx.author.mention} you did not enter a valid user.")
+            return await ctx.send(f"{ctx.author.mention} you didn't provide a valid user.")
 
     # Gets a user's avatar
-    @commands.command(usage='[user]', brief='Gets a user\'s avatar.', description='Gets a user\'s avatar.')
+    @commands.command(usage='[user]', brief='Gets a user\'s avatar.', description='Gets a user\'s avatar. Leave blank to get your own avatar.')
     @commands.guild_only()
     async def avatar(self, ctx, user: discord.Member = None):
         user = user or ctx.author
@@ -42,7 +42,7 @@ class Misc(commands.Cog):
     @avatar.error
     async def avatar_error(self, ctx, error):
         if isinstance(error, commands.BadArgument):
-            return await ctx.send(f"{ctx.author.mention} you did not enter a valid user.")
+            return await ctx.send(f"{ctx.author.mention} you didn't provide a valid user.")
 
 def setup(bot):
     bot.add_cog(Misc(bot))
