b02532b7591df407b1386d064fef7e4958b217e2
Dakski <ghasthunter352@gmail.com>
2020-11-07 13:15:51 +1300

    Added Danbooru search

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -28,20 +28,26 @@ class NSFW(commands.Cog):
                 elif error:
                     return await ctx.send(f"{ctx.author.mention} {error}")
 
+    # Get image from Danbooru
+    @commands.command(aliases=['dan'], usage='[tag]', brief='Get an image from Danbooru with the specified tag(s).', description='Get an image from Danbooru with the specified tag(s). Multiple tags can be appended with + e.g. tag1+tag2. Note: Danbooru searches are limited to only two tags. Leave blank for random image.')
+    @commands.is_nsfw()
+    async def danbooru(self, ctx, tags: str = ''):
+        return await self.process_nsfw(ctx, 'danbooru', tags)
+
     # Get image from Gelbooru
-    @commands.command(usage='[tag]', brief='Get an image from Gelbooru with the specified tag(s).', description='Get an image from Gelbooru with the specified tag(s). Multiple tags can be appended with + e.g. tag1+tag2. Leave blank for random image.')
+    @commands.command(aliases=['gel'], usage='[tag]', brief='Get an image from Gelbooru with the specified tag(s).', description='Get an image from Gelbooru with the specified tag(s). Multiple tags can be appended with + e.g. tag1+tag2+tag... Leave blank for random image.')
     @commands.is_nsfw()
     async def gelbooru(self, ctx, tags: str = ''):
         return await self.process_nsfw(ctx, 'gelbooru', tags)
 
     # Get image from rule34.xxx
-    @commands.command(aliases=['r34'], usage='[tag]', brief='Get an image from rule34.xxx with the specified tag(s).', description='Get an image from rule34.xxx with the specified tag(s). Multiple tags can be appended with + e.g. tag1+tag2. Leave blank for random image.')
+    @commands.command(aliases=['r34'], usage='[tag]', brief='Get an image from rule34.xxx with the specified tag(s).', description='Get an image from rule34.xxx with the specified tag(s). Multiple tags can be appended with + e.g. tag1+tag2... Leave blank for random image.')
     @commands.is_nsfw()
     async def rule34(self, ctx, tags: str = ''):
         return await self.process_nsfw(ctx, 'rule34', tags)
 
     # Deletes the recently requested image
-    @commands.command(aliases=['hateit', 'undo'], brief='Delete the image you recently requested.', description='Delete the image you recently requested.')
+    @commands.command(aliases=['del', 'undo'], brief='Delete the image you recently requested.', description='Delete the image you recently requested.')
     @commands.is_nsfw()
     async def deletethis(self, ctx):
         try:
@@ -53,6 +59,7 @@ class NSFW(commands.Cog):
             del self.image_cache[ctx.author.id]
             return
 
+    @danbooru.error
     @gelbooru.error
     @rule34.error
     @deletethis.error
--- core/nsfw_core.py
+++ core/nsfw_core.py
@@ -9,14 +9,17 @@ __all__ = ['get_image']
 # Function for getting the site parameters
 async def get_site_params(site: str):
     site_url = {
+        'danbooru': 'https://danbooru.donmai.us',
         'gelbooru': 'https://gelbooru.com/index.php?',
         'rule34': 'https://rule34.xxx/index.php?'
     }[site]
     tag_param = {
+        'danbooru': '/post/index.xml?limit=100&tags=',
         'gelbooru': 'page=dapi&s=post&q=index&tags=',
         'rule34': 'page=dapi&s=post&q=index&tags='
     }[site]
     id_param = {
+        'danbooru': '/posts/',
         'gelbooru': 'page=post&s=view&id=',
         'rule34': 'page=post&s=view&id='
     }[site]
@@ -29,15 +32,19 @@ async def get_image(site: str, tags: str):
         r = requests.get(site_url + tag_param + tags.lower())
         if r.status_code == 200:
             soup = BeautifulSoup(r.text, 'lxml')
-            if int(soup.find('posts')['count']) == 0:
+            post_count = soup.find('posts').find_all('post')
+            if (site != 'danbooru' and int(soup.find('posts')['count']) == 0) or len(post_count) == 0:
                 return None, None, 'the specified tag returned no results.'
             else:
-                post_count = soup.find('posts').find_all('post')
                 post = post_count[random.randint(0, len(post_count)-1)]
                 file_url = post['file_url']
                 post_link = site_url + id_param + post['id']
                 return file_url, post_link, None
         else:
-            return None, None, f'couldn\'t process the request. Error code: {r.status_code}.'
+            if site == 'danbooru' and r.status_code == 422:
+                error_message = 'Danbooru searches are limited to two tags.'
+            else:
+                error_message = f'couldn\'t process the request. Status code: {r.status_code} {r.reason}.'
+            return None, None, error_message
     except requests.exceptions.RequestException as e:
         return None, None, f'**ERROR:** {type(e).__name__} - {e}'
