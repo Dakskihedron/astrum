55ca47fea1b4d8b91e4bd6d488bc10cc37a5d0ed
Dakskihedron <ghasthunter352@gmail.com>
2021-04-25 03:37:26 +1200

    Restructuring

---
+++ src/cogs/info.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+from utils import default
+
+class Info(commands.Cog):
+    """Commands to do with information."""
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    @commands.guild_only()
+    async def ping(self, ctx):
+        """Show the bot's response time and latency."""
+        m = await ctx.send("**Pinging...**")
+        embed = discord.Embed()
+        embed.add_field(name='Response', value=f"{(m.created_at - ctx.message.created_at).total_seconds() * 1000} ms")
+        embed.add_field(name='Latency', value=f"{self.bot.latency * 1000:.2f} ms")
+        await m.edit(content=None, embed=embed)
+
+    @commands.command()
+    @commands.guild_only()
+    async def source(self, ctx):
+        """Link to bot's source code on GitHub."""
+        return await ctx.send(default.github_link)
+
+def setup(bot):
+    bot.add_cog(Info(bot))
