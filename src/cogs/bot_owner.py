0a3dfbe0d32e22370f24f7aea677bf740611f866
Dakskihedron <ghasthunter352@gmail.com>
2021-04-25 14:23:56 +1200

    Renamed cog and added cog list cmd

--- src/cogs/bot_owner.py
+++ src/cogs/bot_owner.py
@@ -3,19 +3,31 @@ from discord.ext import commands
 
 from main import initial_extensions
 
-class Admin(commands.Cog):
-    """Commands restricted to specific users."""
+class BotOwner(commands.Cog):
+    """Commands related to cogs. Only usable by the bot owner."""
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name='listcogs')
+    @commands.guild_only()
+    @commands.is_owner()
+    async def list_cogs(self, ctx):
+        """List all cogs."""
+        nl = '\n'
+        ext_list = []
+        for extension in initial_extensions:
+            ext_list.append(extension.replace('cogs.', ''))
+        return await ctx.send(f"```\n{nl.join(ext_list)}\n```")
+        
     @commands.command(name='load')
     @commands.guild_only()
     @commands.is_owner()
-    async def load_cog(self, ctx, *, cog : str):
+    async def load_cog(self, ctx, cog: str):
         """Load a specified cog.
 
-        cog: cogs.[str]
+        cog: str
         The name of the cog you want to load."""
+        cog = f'cogs.{cog}'
         try:
             self.bot.load_extension(cog)
         except Exception as e:
@@ -26,13 +38,14 @@ class Admin(commands.Cog):
     @commands.command(name='unload')
     @commands.guild_only()
     @commands.is_owner()
-    async def unload_cog(self, ctx, *, cog : str):
+    async def unload_cog(self, ctx, cog: str):
         """Unload a specified cog.
 
-        cog: cogs.[str]
+        cog: str
         The name of the cog you want to unload."""
-        if cog == 'cogs.admin':
-            return await ctx.send(f"**ERROR:** 'cogs.admin' cannot be unloaded.")
+        cog = f'cogs.{cog}'
+        if cog == 'cogs.bot_owner':
+            return await ctx.send(f"**ERROR:** `cogs.bot_owner` cannot be unloaded.")
         else:
             try:
                 self.bot.unload_extension(cog)
@@ -44,11 +57,12 @@ class Admin(commands.Cog):
     @commands.command(name='reload')
     @commands.guild_only()
     @commands.is_owner()
-    async def reload_cog(self, ctx, *, cog : str):
+    async def reload_cog(self, ctx, cog: str):
         """Reload a specified cog.
 
-        cog: cogs.[str]
+        cog: str
         The name of the cog you want to reload."""
+        cog = f'cogs.{cog}'
         try:
             self.bot.reload_extension(cog)
         except Exception as e:
@@ -77,4 +91,4 @@ class Admin(commands.Cog):
         return await ctx.send("**Successfully reloaded all cogs.**")
 
 def setup(bot):
-    bot.add_cog(Admin(bot))
+    bot.add_cog(BotOwner(bot))
