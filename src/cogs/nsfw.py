876b1821824fbac573d208d69cec1304861499be
Dakskihedron <ghasthunter352@gmail.com>
2021-05-23 14:11:29 +1200

    Minor changes

--- src/cogs/nsfw.py
+++ src/cogs/nsfw.py
@@ -14,18 +14,18 @@ class NSFW(commands.Cog):
     async def process_nsfw(self, ctx, site: str, tags: str):
         for x in nsfw_blacklist:
             if x in tags.lower():
-                return await ctx.send("One of the tags you have specified is blacklisted.")
+                return await ctx.reply("One of the tags you have specified is blacklisted.")
             else:
                 file_url, post_link, error = await get_image(site, tags)
                 if file_url and post_link:
-                    embed = discord.Embed()
+                    embed = discord.Embed(colour = discord.Colour.blurple())
                     embed.description = f'[Post Link]({post_link})'
                     embed.set_image(url=file_url)
                     msg = await ctx.send(embed=embed)
                     self.image_cache[ctx.author.id] = msg.id
                     return
                 elif error:
-                    return await ctx.send(error)
+                    return await ctx.reply(error)
 
     @commands.command(aliases=['dan'])
     @commands.is_nsfw()
@@ -55,26 +55,22 @@ class NSFW(commands.Cog):
         The tag(s) you want to search. Multiple tags can be appended using plus signs. Spaces in tags must be replaced with an underscore. Leave blank for a random image."""
         return await self.process_nsfw(ctx, 'rule34', tags)
 
-    @commands.command(aliases=['del', 'undo'])
+    @commands.command(name='undo', aliases=['delete', 'del'])
     @commands.is_nsfw()
-    async def deletethis(self, ctx):
+    async def delete_post(self, ctx):
         """Remove the image you recently requested."""
         try:
             msg = await ctx.channel.fetch_message(self.image_cache[ctx.author.id])
         except:
-            return await ctx.send("There is no image to delete.")
+            return await ctx.reply("There is no image to delete.")
         else:
             await msg.delete()
             del self.image_cache[ctx.author.id]
             return
 
-    @danbooru.error
-    @gelbooru.error
-    @rule34.error
-    @deletethis.error
-    async def nsfw_error(self, ctx, error):
+    async def cog_command_error(self, ctx, error):
         if isinstance(error, commands.NSFWChannelRequired):
-            return await ctx.send("Command can only used in a NSFW channel.")
+            return await ctx.reply("Command can only used in a NSFW channel.")
 
 def setup(bot):
     bot.add_cog(NSFW(bot))
