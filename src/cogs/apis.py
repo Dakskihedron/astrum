9c1e2495fe85a7a4f7aa245325b65307634a31fb
Dakskihedron <ghasthunter352@gmail.com>
2021-05-23 23:14:37 +1200

    Danbooru command added

--- src/cogs/apis.py
+++ src/cogs/apis.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 import os
-import dotenv
+import random
 import requests
 import re
 
@@ -13,6 +13,7 @@ class APIs(commands.Cog):
     """Commands related to APIs."""
     def __init__(self, bot):
         self.bot = bot
+        self.image_cache = dict()
 
     @commands.command()
     @commands.guild_only()
@@ -44,9 +45,66 @@ class APIs(commands.Cog):
                 return await ctx.send(f'https://youtu.be/{url.group(1)}')
             else:
                 return await ctx.send(data['url'])
-        except requests.exceptions.RequestException as e:
+        except requests.exceptions.RequestException:
             data = requests.get(url).json()
-            return await ctx.reply(f"{r.status_code}: {data['msg']}")
+            await ctx.reply(f"{r.status_code}: {data['msg']}")
+
+    @commands.command()
+    @commands.is_nsfw()
+    @commands.guild_only()
+    async def danbooru(self, ctx, tags=''):
+        """Show an image from Danbooru.
+
+        tag: str
+        Optional tag(s) for searching specific images."""
+        blacklist = [
+            "loli",
+            "lolicon",
+            "shota",
+            "shotacon",
+            "cub",
+            "child",
+            "beastiality",
+            "incest",
+            "rape"
+        ]
+        for x in blacklist:
+            if x in tags.lower():
+                return await ctx.reply("The specified tag(s) are blacklisted.")
+        else:
+            url = f'https://danbooru.donmai.us/posts.json?limit=200&tags={tags}'
+            try:
+                r = requests.get(url)
+                r.raise_for_status()
+                data = r.json()
+                if len(data) == 0:
+                    return await ctx.reply("The specified tag(s) returned no results.")
+                else:
+                    post = random.choice(data)
+                    msg = await ctx.send(post['file_url'])
+                    self.image_cache[ctx.author.id] = msg.id
+                    return
+            except requests.exceptions.RequestException:
+                data = requests.get(url).json()
+                await ctx.reply(f"{r.status_code}: {data['message']}")
+
+    @commands.command()
+    @commands.is_nsfw()
+    @commands.guild_only()
+    async def undo(self, ctx):
+        """Remove your recently requested image."""
+        request = self.image_cache[ctx.author.id]
+        try:
+            msg = await ctx.channel.fetch_message(request)
+        except:
+            return await ctx.reply("There is no image to remove.")
+        else:
+            await msg.delete()
+            del request
+
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error, commands.NSFWChannelRequired):
+            return await ctx.reply("Command can only be used in a NSFW channel.")
 
 def setup(bot):
     bot.add_cog(APIs(bot))
