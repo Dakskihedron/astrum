083d8536d1626e785360b29b2eb8d5e928c5af65
Dakskihedron <ghasthunter352@gmail.com>
2021-05-30 06:48:06 +1200

    Changes to how cogs are loaded

--- src/main.py
+++ src/main.py
@@ -6,8 +6,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from datetime import datetime
+import cogs.utils.default as default
 
-# Get environment variables
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 token = os.getenv('DISCORD_TOKEN')
 prefix = os.getenv('BOT_PREFIX')
 
@@ -22,29 +30,18 @@ bot = commands.Bot(
     )
 )
 
-def date(target):
-    return target.strftime("%a, %d %b %Y @ %I:%M:%S %p")
-
-# On ready log
 @bot.event
 async def on_ready():
-    print(f"{bot.user} logged in on {len(bot.guilds)} servers on {date(datetime.now())}")
+    print(f"{bot.user} logged in on {len(bot.guilds)} servers on {default.date(datetime.now())}")
 
-# Load the following extensions
-initial_extensions = (
-    "cogs.bot_owner",
-    "cogs.apis",
-    "cogs.fun",
-    "cogs.info",
-    "cogs.misc",
-    "cogs.roles"
-)
-
-for extension in initial_extensions:
-    try:
-        bot.load_extension(extension)
-        print(f"Successfully loaded extension {extension}")
-    except Exception as e:
-        print(f"Failed to load extension {extension}")
+if __name__ == '__main__':
+    for ext in os.listdir('src/cogs'):
+        if ext.endswith('.py'):
+            ext = ext[:-3]
+            try:
+                bot.load_extension(f'cogs.{ext}')
+                print(f"Successfully loaded extension '{ext}'")
+            except Exception as e:
+                print(f"Failed to load extension '{ext}'\n{type(e).__name__}: {e}")
 
 bot.run(token)
