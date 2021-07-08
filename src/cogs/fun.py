55003c05d68960b62e593b196653ab9e5efa7466
Dakskihedron <ghasthunter352@gmail.com>
2021-07-08 20:50:59 +1200

    Add hangman command

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -1,11 +1,15 @@
+import discord
 from discord.ext import commands
 import random
+import asyncio
+import cogs.utils.functions as functions
 
 
 class Fun(commands.Cog):
     """Commands that do fun stuff."""
     def __init__(self, bot):
         self.bot = bot
+        self.playing_list = []
 
     @commands.command(name='coin')
     @commands.guild_only()
@@ -78,6 +82,86 @@ class Fun(commands.Cog):
             'Very doubtful',
             ]
         await ctx.reply(f"{random.choice(responses)}.")
+
+    @commands.command()
+    @commands.guild_only()
+    async def hangman(self, ctx):
+        """Start a game of hangman."""
+        if ctx.author.id not in self.playing_list:
+            self.playing_list.append(ctx.author.id)
+        else:
+            return await ctx.reply("Game already running.")
+        data, status = await functions.get_data(
+            'https://random-word-api.herokuapp.com/word?number=1'
+            )
+        if data and status:
+            return await ctx.reply(f"{status}: {data}")
+        else:
+            word = data[0]
+        guesses = 8
+        char_list = []
+        word_guessed = list(len(word) * '_')
+        embed = discord.Embed(colour=discord.Colour.blurple())
+        embed.set_author(name=f'Guesses Left: {guesses}')
+        embed.add_field(
+            name=f"`{' '.join(word_guessed)}`",
+            value='\u200b'
+            )
+        embed.set_footer(text=f'End game with {ctx.prefix}end')
+        await ctx.reply(embed=embed)
+
+        def check(message):
+            return message.author == ctx.message.author
+
+        while guesses != 0:
+            try:
+                char = await self.bot.wait_for(
+                    'message',
+                    timeout=30,
+                    check=check
+                    )
+                char = char.content.lower()
+            except asyncio.TimeoutError:
+                self.playing_list.pop(self.playing_list.index(ctx.author.id))
+                return await ctx.reply(
+                    "Game timed out. "
+                    f"The word was {word}."
+                    )
+            if char == f'{ctx.prefix}end':
+                break
+            elif char in char_list or char in word_guessed:
+                await ctx.reply("Duplicate letter.")
+            elif char in word:
+                for index, x in enumerate(word):
+                    if x == char:
+                        word_guessed[index] = char
+                    embed.set_field_at(
+                        index=0,
+                        name=f"`{' '.join(word_guessed)}`",
+                        value=' '.join(char_list) or '\u200b'
+                        )
+                await ctx.reply(embed=embed)
+                if ''.join(word_guessed) == word:
+                    self.playing_list.pop(
+                        self.playing_list.index(ctx.author.id)
+                        )
+                    return await ctx.reply("You figured out the word!")
+            elif not char == f'{ctx.prefix}hangman':
+                guesses -= 1
+                if guesses == 0:
+                    break
+                char_list.append(char)
+                embed.set_author(name=f'Guesses Left: {guesses}')
+                embed.set_field_at(
+                    index=0,
+                    name=f"`{' '.join(word_guessed)}`",
+                    value=' '.join(char_list)
+                    )
+                await ctx.reply(embed=embed)
+        self.playing_list.pop(self.playing_list.index(ctx.author.id))
+        return await ctx.reply(
+            f"Game ended. The word was {word}."
+            )
 
 
 def setup(bot):
